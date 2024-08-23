23ec8c28bf58a9b40d4603539a000711f6edc09a
smoke0030 <ft.priest@gmail.com>
2024-08-23 14:48:41 +0300

    Initial Commit

---
+++ EffectiveMobileTest/EffectiveMobileTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveMobileTestApp.swift
+//  EffectiveMobileTest
+//
+//  Created by Сергей on 23.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveMobileTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
